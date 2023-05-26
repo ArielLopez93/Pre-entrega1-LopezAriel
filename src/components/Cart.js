1013ef38a9ab2137ca5c31a9904caa95fe940a03
Ariel <ariel.n.lopez@hotmail.com>
2023-05-26 14:11:09 -0300

    componentes finalizados

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,36 @@
 import React from "react"
 import { useCartContext } from "../CartContext"
+import { getFirestore, addDoc, collection } from "firebase/firestore"
 import { Link } from "react-router-dom"
 import ItemCart from "./ItemCart"
 
 const Cart = () => {
   const { cart, totalPrice} = useCartContext()
+
+  const order ={
+    buyer : {
+      name : 'Ariel',
+      email : 'ArielLopez@coder.com',
+      telefono : '457751384',
+      direccion : 'Avellaneda'
+    },
+
+    items : cart.map(peli=> ({ id: peli.id, title: peli.title, price: peli.price, cantidad: peli.quantity})),
+    totalPrice: totalPrice(),
+
+  }
+
+
+    const handleClick =() =>{
+      const db = getFirestore()
+      const orderCollection = collection (db, 'orders')
+      addDoc (orderCollection, order)
+      .then(({id})=>console.log(id))
+    }
+
+
   
-  if (cart.length ==0){
+  if (cart.length === 0){
     return(
       <>
        <p>No agregaste nada al carrito</p>
@@ -23,6 +47,7 @@ const Cart = () => {
     <p>
       Total: ${totalPrice()}
     </p>
+    <butto onClick={handleClick}>Comprar</butto>
     </>
     )
 }
